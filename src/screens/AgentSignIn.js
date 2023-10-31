507b8c9ff77c64409017ad74ed184893aeb87fde
Dhrooven <j.dhruv315@gmail.com>
2023-11-01 00:43:23 +0530

    Added React-Form-Hook

--- src/screens/AgentSignIn.js
+++ src/screens/AgentSignIn.js
@@ -2,14 +2,12 @@ import React from 'react';
 import {View, SafeAreaView, Text, StyleSheet} from 'react-native';
 
 const AgentSignIn = () => {
-    return (
-        <SafeAreaView>
-            <View>
-                <
-            </View>
-        </SafeAreaView>
-    );
-}
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({});
 
